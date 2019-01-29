841b43d02311e26cd09da846a224c1be251f9e4a
Gabriel Florit <gabrielflorit@gmail.com>
2019-01-29 01:21:05 -0500

    Add HashRouter to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './containers/App.js'
+import { HashRouter } from 'react-router-dom'
+import Root from './components/Root.js'
 import store from './store/store.js'
 
 window.script8 = window.script8 || {}
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <HashRouter>
+    <Root store={store} />
+  </HashRouter>,
   document.getElementById('root')
 )
