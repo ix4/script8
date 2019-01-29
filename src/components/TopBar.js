9ff4b53698b4ff78b44088024b885beda8b441af
Gabriel Florit <gabrielflorit@gmail.com>
2019-01-29 01:20:47 -0500

    Remove unnecessary hideMenu prop from TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Updater from '../containers/Updater.js'
 import Menu from '../containers/Menu.js'
 
-const TopBar = ({ hideMenu }) => (
+const TopBar = () => (
   <div className='TopBar'>
     <Updater />
     <Menu />
   </div>
 )
-
-TopBar.propTypes = {
-  hideMenu: PropTypes.bool
-}
 
 export default TopBar
